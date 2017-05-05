33c90d527006b01247ebbd9ecbce183416f2cd8c
smchow <sunita.m.chowdhury@gmail.com>
2017-05-05 18:27:57 -0400

    DB model changes - Projects

--- models/students.js
+++ models/students.js
@@ -1,4 +1,5 @@
-// Requiring bcrypt for password hashing. 
+// Requiring bcrypt for password hashing. Using the bcrypt-nodejs version as the regular bcrypt module
+// sometimes causes errors on Windows machines
 var bcrypt = require("bcrypt-nodejs");
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
@@ -17,40 +18,37 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false,
-  	}, 
-	username: {
-	   type: DataTypes.STRING,
-	   allowNull: false,
-	},
-	country: {
-		type: DataTypes.STRING,
-	    allowNull: false,
-	},
-	state: {
-	  	type: DataTypes.STRING,
-	},
-	city: {
-	    type: DataTypes.STRING,
-	    allowNull: false,
-        validate: {
-          len:[1,200]
-        }
-	},
+    }, 
+  username: {
+     type: DataTypes.STRING,
+     allowNull: false,
+  },
+  longitude: {
+    type: DataTypes.STRING,
+  },
+  latitude: {
+    type: DataTypes.STRING,
+  },
+   gmtoffset: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+    timezone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    }
   }, {
-    // Creating a custom method for our User model.
+    // Creating a custom method for our User model. This will check if an unhashed password entered by
+    // The user can be compared to the hashed password stored in our database
     instanceMethods: {
       validPassword: function(password) {
         return bcrypt.compareSync(password, this.password);
       }
     },
+    // Hooks are automatic methods that run during various phases of the User Model lifecycle
+    // In this case, before a User is created, we will automatically hash their password
     
-    validate: {
-    	statesUS:  function() {
-    		if (this.country == "United States of America" && this.state.trim() == "") {
-    			throw new Error("In the US state is required")
-    		}
-    	},
-    },
+    
 
     hooks: {
       beforeCreate: function(user, options, cb) {
